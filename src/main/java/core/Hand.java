451bcf5ce321985edae6339449d1bfe25f0ecc76
LayneKM <laynemikan@gmail.com>
2018-10-03 12:47:17 -0400

    [refactor:Hand] Update formatting

--- src/main/java/core/Hand.java
+++ src/main/java/core/Hand.java
@@ -44,11 +44,11 @@ public class Hand {
     
     private void sort() {
         
-        //map colour to its order
+        // Map colour to its order
         Map<Character, Integer> order = new HashMap<Character, Integer>();
         order.put('R', 1); order.put('G', 2); order.put('B', 3); order.put('O', 4);
         
-        //sort by colour
+        // Sort by colour
         int n = hand.size();
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
@@ -57,12 +57,12 @@ public class Hand {
                 if (colourVal > order.get(hand.get(j+1).getColour())) {
                     Tile temp = hand.get(j);
                     hand.set(j, hand.get(j+1));
-                    hand.set(j+1, temp);
+                    hand.set(j + 1, temp);
                 }
             }
         }
         
-        //sort by value within colour
+        // Sort by value within colour
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 int colourVal = order.get(hand.get(j).getColour());
@@ -71,7 +71,7 @@ public class Hand {
                 if (value > hand.get(j+1).getValue() && colourVal == order.get(hand.get(j+1).getColour())) {
                     Tile temp = hand.get(j);
                     hand.set(j, hand.get(j+1));
-                    hand.set(j+1, temp);
+                    hand.set(j + 1, temp);
                 }
             }
         }
