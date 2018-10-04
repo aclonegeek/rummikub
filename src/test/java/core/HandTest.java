6c9ab5d9f81e21104e1af4c29ead49838fbf2230
LayneKM <laynemikan@gmail.com>
2018-10-04 11:17:27 -0400

    [refactor:HandTest] Refactored to use global Colour enum

--- src/test/java/core/HandTest.java
+++ src/test/java/core/HandTest.java
@@ -1,5 +1,6 @@
 package core;
 
+import core.Globals.Colour;
 import junit.framework.TestCase;
 
 public class HandTest extends TestCase {
@@ -10,14 +11,14 @@ public class HandTest extends TestCase {
         assertEquals(0, hand.getSize());
         assertEquals("[]", hand.toString());
         
-        Tile tile1 = new Tile('R', 1);
-        Tile tile2 = new Tile('R', 2);
-        Tile tile3 = new Tile('G', 3);
-        Tile tile4 = new Tile('G', 5);
-        Tile tile5 = new Tile('B', 1);
-        Tile tile6 = new Tile('B', 10);
-        Tile tile7 = new Tile('O', 7);
-        Tile tile8 = new Tile('O', 13);
+        Tile tile1 = new Tile(Colour.RED, 1);
+        Tile tile2 = new Tile(Colour.RED, 2);
+        Tile tile3 = new Tile(Colour.GREEN, 3);
+        Tile tile4 = new Tile(Colour.GREEN, 5);
+        Tile tile5 = new Tile(Colour.BLUE, 1);
+        Tile tile6 = new Tile(Colour.BLUE, 10);
+        Tile tile7 = new Tile(Colour.ORANGE, 7);
+        Tile tile8 = new Tile(Colour.ORANGE, 13);
         
         hand.add(tile7);
         hand.add(tile3);
@@ -28,7 +29,7 @@ public class HandTest extends TestCase {
         hand.add(tile2);
         hand.add(tile6);
         assertEquals(8, hand.getSize());
-        assertEquals("[R1, R2, G3, G5, B1, B10, O7, O13]", hand.toString());
+        assertEquals("[R1, R2, B1, B10, G3, G5, O7, O13]", hand.toString());
     }
     
     // Tests remove()
@@ -36,9 +37,9 @@ public class HandTest extends TestCase {
     public void testRemove() {
         Hand hand = new Hand();
         
-        Tile tile1 = new Tile('R', 1);
-        Tile tile2 = new Tile('R', 2);
-        Tile tile3 = new Tile('G', 3);
+        Tile tile1 = new Tile(Colour.RED, 1);
+        Tile tile2 = new Tile(Colour.RED, 2);
+        Tile tile3 = new Tile(Colour.GREEN, 3);
         hand.add(tile1);
         hand.add(tile2);
         hand.add(tile3);
