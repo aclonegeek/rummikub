32a5e9251e8059feade301ca8ec4f517390ddf0a
Altin R <altin.rex@gmail.com>
2018-10-03 22:35:54 -0400

    [test:TileTest] Add test case for new colour type in constructor

--- src/test/java/core/TileTest.java
+++ src/test/java/core/TileTest.java
@@ -1,15 +1,16 @@
 package core;
 
+import core.Globals.Colour;
 import junit.framework.TestCase;
 
 public class TileTest extends TestCase {
     public void testTile() {
-        Tile t1 = new Tile('R', 1);
+        Tile t1 = new Tile(Colour.RED, 1);
         assertNotNull(t1);
     }
     
     public void testToString() {
-        Tile t1 = new Tile('O', 6);
+        Tile t1 = new Tile(Colour.ORANGE, 6);
         assertEquals("O6", t1.toString());
     }
 }
