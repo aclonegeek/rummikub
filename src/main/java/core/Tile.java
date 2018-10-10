a8cf52ec83d72610052bd135733dd340e3996dc3
Altin R <altin.rex@gmail.com>
2018-10-10 19:01:43 -0400

    [refactor:Tile] Add onTable variable with getter/setter

--- src/main/java/core/Tile.java
+++ src/main/java/core/Tile.java
@@ -5,10 +5,12 @@ import core.Globals.Colour;
 public class Tile {
     private Colour colour;
     private int value;
+    private boolean onTable;
     
     public Tile (Colour colour, int value) {
         this.colour = colour;
         this.value = value;
+        this.onTable = false;
     }
     
     public Colour getColour() {
@@ -17,6 +19,14 @@ public class Tile {
     
     public int getValue() {
         return this.value;
+    }
+    
+    public boolean isOnTable() {
+        return this.onTable;
+    }
+    
+    public void setOnTable() {
+        this.onTable = true;
     }
     
     @Override
