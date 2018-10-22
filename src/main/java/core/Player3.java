9ddee654ea98749fb9681d66c6d69253ca7c21ed
Randy Taylor <tehgecKozzz@gmail.com>
2018-10-22 19:49:26 -0400

    [code:Player3] Implement PlayerObserver for Player3 class

--- src/main/java/core/Player3.java
+++ src/main/java/core/Player3.java
@@ -1,10 +1,22 @@
 package core;
 
-public class Player3 extends Player {
+public class Player3 extends Player implements PlayerObserver {
+    private int lowestHandCount = -1;
+
     public Player3() {
         this.playBehaviour = new Strategy3();
     }
-    
+
+    public void update(int lowestHandCount) {
+        if (this.lowestHandCount == -1 || lowestHandCount < this.lowestHandCount) {
+            this.lowestHandCount = lowestHandCount;
+        }
+    }
+
+    public int getLowestHandCount() {
+        return this.lowestHandCount;
+    }
+
     public String toString() {
         return "Player 3:\n# tiles: " + hand.getSize() + "\n\n";
     }
