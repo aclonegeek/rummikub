39df280885adbf1bb2c767daa5b407d1ecd6f576
Randy Taylor <tehgecKozzz@gmail.com>
2018-10-22 19:51:09 -0400

    [code:Player1] Implement PlayerSubject for Player1 class

--- src/main/java/core/Player1.java
+++ src/main/java/core/Player1.java
@@ -1,11 +1,32 @@
 package core;
 
-public class Player1 extends Player {
+import java.util.ArrayList;
+
+public class Player1 extends Player implements PlayerSubject {
+    ArrayList<PlayerObserver> observers;
+
     public Player1() {
+        this.observers = new ArrayList<>();
         this.playBehaviour = new Strategy1();
     }
-    
+
+	public void registerObserver(PlayerObserver observer) {
+		this.observers.add(observer);
+	}
+
+	public void removeObserver(PlayerObserver observer) {
+        this.observers.removeIf(o -> observer.equals(o));
+	}
+
+	public void notifyObservers() {
+        this.observers.forEach(o -> o.update(this.getHandSize()));
+	}
+
+    public ArrayList<PlayerObserver> getObservers() {
+        return this.observers;
+    }
+
     public String toString() {
-        return "Player 1:\n# tiles: " + hand.getSize() + "\n\n";
+        return "Player 1:\n# tiles: " + this.hand.getSize() + "\n\n";
     }
 }
