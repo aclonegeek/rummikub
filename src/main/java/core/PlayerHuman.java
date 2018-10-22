c68dbcb97e268d24e470b665f3ad81a53bdb2f00
Randy Taylor <tehgecKozzz@gmail.com>
2018-10-22 19:50:09 -0400

    [code:PlayerHuman] Implement PlayerSubject for PlayerHuman class

--- src/main/java/core/PlayerHuman.java
+++ src/main/java/core/PlayerHuman.java
@@ -1,11 +1,32 @@
 package core;
 
-public class PlayerHuman extends Player {
+import java.util.ArrayList;
+
+public class PlayerHuman extends Player implements PlayerSubject {
+    private ArrayList<PlayerObserver> observers;
+
     public PlayerHuman() {
+        this.observers = new ArrayList<>();
         this.playBehaviour = new StrategyHuman();
     }
-    
+
+	public void registerObserver(PlayerObserver observer) {
+		this.observers.add(observer);
+	}
+
+	public void removeObserver(PlayerObserver observer) {
+        this.observers.removeIf(o -> observer.equals(o));
+	}
+
+	public void notifyObservers() {
+        this.observers.forEach(o -> o.update(this.getHandSize()));
+	}
+
+    public ArrayList<PlayerObserver> getObservers() {
+        return this.observers;
+    }
+
     public String toString() {
-        return "You:\n# tiles: " + hand.getSize() + "\n" + hand.toString() + "\n\n";
+        return "You:\n# tiles: " + this.hand.getSize() + "\n" + this.hand.toString() + "\n\n";
     }
 }
