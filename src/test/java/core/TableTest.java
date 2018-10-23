311cdbbaf9b834370eb9bda0109538973502b8ce
Altin R <altin.rex@gmail.com>
2018-10-22 21:19:09 -0400

    [fix:TableTest] testRemoveObserver now fully tests all branches

--- src/test/java/core/TableTest.java
+++ src/test/java/core/TableTest.java
@@ -1,7 +1,6 @@
 package core;
 
 import java.util.ArrayList;
-
 import core.Globals.Colour;
 import junit.framework.TestCase;
 
@@ -39,28 +38,48 @@ public class TableTest extends TestCase {
     
     public void testRemoveObserver() {
         Table table = new Table();
-        
-        // Create list of melds
         ArrayList<Meld> melds = new ArrayList<>();
-        Meld meld1 = new Meld();
-        Meld meld2 = new Meld();
-        melds.add(meld1);
-        melds.add(meld2);
+        
+        // Meld to be added to ArrayList of Melds
+        Meld meld = new Meld();
+        
+        Player player1 = new Player1();
+        table.registerObserver(player1);
+        
+        // Player1's workspace should be size 0
+        assertEquals(0, player1.getWorkspace().size());
+        
+        ArrayList<Tile> set = new ArrayList<>();
+        set.add(new Tile(Colour.RED, 7));
+        set.add(new Tile(Colour.BLUE, 7));
+        set.add(new Tile(Colour.GREEN, 7));
+        set.add(new Tile(Colour.ORANGE, 7));
+        meld.addTile(set);
+        melds.add(meld);
         
         table.setState(melds);
         
-        // Create new player and register it as a table observer
-        Player player1 = new Player1();
-        ArrayList<Meld> player1Workspace = player1.getWorkspace();
-        assertEquals(0, player1Workspace.size());
-        table.registerObserver(player1);
+        // Player1's workspace should be size 1
+        assertEquals(1, player1.getWorkspace().size());
         
+        // Remove Player1 as an observer
         table.removeObserver(player1);
-        table.notifyObservers();
         
-        // Player 2's workspace shouldn't have the new table state after notifying
-        player1Workspace = player1.getWorkspace();
-        assertEquals(0, player1Workspace.size());
+        // Create a new meld and add it to the table
+        meld = new Meld();
+        ArrayList<Tile> run = new ArrayList<>();
+        run.add(new Tile(Colour.RED, 1));
+        run.add(new Tile(Colour.RED, 2));
+        run.add(new Tile(Colour.RED, 3));
+        meld.addTile(run);
+        melds.add(meld);
+
+        // Player1's should still only have 1 meld in their workspace
+        assertEquals(1, player1.getWorkspace().size());
+        
+        // Test removeObserver if the player doesn't exist
+        Player player3 = new Player3();
+        table.removeObserver(player3);
     }
     
     public void testSetState() {
@@ -75,12 +94,11 @@ public class TableTest extends TestCase {
         set.add(new Tile(Colour.BLUE, 7));
         set.add(new Tile(Colour.GREEN, 7));
         set.add(new Tile(Colour.ORANGE, 7));
-        
-        // Make sure meld1 is a valid meld
         meld1.addTile(set);
+        
         assertTrue(meld1.isValidMeld());
         
-        // Split the meld at index 1 to make one invalid (NONE) meld {R7} and one valid meld {B7, G7, O7} and add to melds
+        // Split the meld to make an invalid meld 
         Meld invalidMeld = meld1.splitMeld(1);
         melds.add(invalidMeld);
         melds.add(meld1);
