b61138d39125872069c0c91e7d83cb45d17152c9
LayneKM <laynemikan@gmail.com>
2018-10-24 14:37:41 -0400

    [code:Strategy2:Strategy3] Implement Strategy2 and Strategy3 initial move methods

--- src/main/java/core/Strategy3.java
+++ src/main/java/core/Strategy3.java
@@ -12,6 +12,78 @@ public class Strategy3 implements PlayBehaviour {
     
     // Plays as few melds as it can using only the tiles in its hand and only if the total of the melds is >= 30
     public ArrayList<Meld> determineInitialMove(ArrayList<Meld> workspace, Hand hand) {
+        // For each tile in hand, add it to a new meld and attempt to add it to every preceding meld
+        ArrayList<Meld> melds1 = new ArrayList<>();
+        for (int i = 0; i < hand.getSize(); i++) {
+            ArrayList<Tile> tiles = new ArrayList<>();
+            tiles.add(hand.getTile(i));
+            Meld meld = new Meld();
+            meld.addTile(tiles);
+            melds1.add(meld);
+            for (Meld tempMeld : melds1) {
+                tempMeld.addTile(tiles);
+            }      
+        }
+        
+        // If there are melds with >= 30 points, add larget to workspace and return
+        ArrayList<Meld> filteredMelds = new ArrayList<>();
+        for (Meld tempMeld : melds1) {
+            if (tempMeld.getValue() >= 30) {
+                filteredMelds.add(tempMeld);
+            }
+        }
+
+        if (filteredMelds.size() > 0) {
+            Meld largestMeld = filteredMelds.get(0);
+            for (Meld tempMeld : filteredMelds) {
+                if (tempMeld.getValue() > largestMeld.getValue()) {
+                    largestMeld = tempMeld;
+                }
+            }
+            workspace.add(largestMeld);
+            return workspace;
+        }
+        
+        // Otherwise, add as few melds as possible to workspace such that total points >= 30
+        int totalTileValue = 0;
+        boolean hasMelds = true;
+        while (hasMelds) {
+            ArrayList<Meld> melds2 = new ArrayList<>();
+            for (int i = 0; i < hand.getSize(); i++) {
+                ArrayList<Tile> tiles = new ArrayList<>();
+                tiles.add(hand.getTile(i));
+                Meld meld = new Meld();
+                meld.addTile(tiles);
+                melds2.add(meld);  
+                for (Meld tempMeld : melds2) {
+                    tempMeld.addTile(tiles);
+                }
+            }
+
+            // Find meld with greatest value
+            Meld greatestMeld = melds2.get(0);
+            for (Meld tempMeld : melds2) {
+                if ((tempMeld.isValidMeld() && !greatestMeld.isValidMeld()) || 
+                    (tempMeld.getValue() > greatestMeld.getValue() && tempMeld.isValidMeld())) {
+                    greatestMeld = tempMeld;
+                }
+            }
+           
+            // If at any point totalTileValue is >= 30, we've found the fewest melds
+            if (greatestMeld.isValidMeld()) {
+                workspace.add(greatestMeld);
+                totalTileValue += greatestMeld.getValue();
+                if (totalTileValue >= 30) {
+                    return workspace;
+                }
+                for (int j = 0; j < greatestMeld.getSize(); j++) {
+                    hand.remove(greatestMeld.getTile(j));
+                }
+            } else {
+                hasMelds = false;
+            }
+        }
+        
         return null;
     }
     
