d2c521d716d8bcae0c5dbeb261fb90d812c0f193
LayneKM <laynemikan@gmail.com>
2018-10-27 10:44:56 -0400

    [code:Strategy2:Strategy3:StrategyHuman] Removed code, will be reimplemented later using PlayBehaviour's methods

--- src/main/java/core/StrategyHuman.java
+++ src/main/java/core/StrategyHuman.java
@@ -1,20 +1,5 @@
 package core;
 
-import java.util.ArrayList;
-
-public class StrategyHuman implements PlayBehaviour {
-    public ArrayList<Meld> determineMove(ArrayList<Meld> workspace, Hand hand, boolean initialMove) {
-        if (initialMove) {
-            return determineInitialMove(workspace, hand);
-        }
-        return determineRegularMove(workspace, hand);
-    }
-    
-    public ArrayList<Meld> determineInitialMove(ArrayList<Meld> workspace, Hand hand) {
-        return null;
-    }
-    
-    public ArrayList<Meld> determineRegularMove(ArrayList<Meld> workspace, Hand hand) {
-        return null;
-    }
+public class StrategyHuman extends PlayBehaviour {
+ 
 }
