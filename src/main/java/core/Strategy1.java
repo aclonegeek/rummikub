2339320091c41ee546b115c77fc9cf63ca74defc
LayneKM <laynemikan@gmail.com>
2018-10-27 10:42:21 -0400

    [refactor:Strategy1] Update to use methods inherited from PlayBehaviour

--- src/main/java/core/Strategy1.java
+++ src/main/java/core/Strategy1.java
@@ -2,80 +2,25 @@ package core;
 
 import java.util.ArrayList;
 
-public class Strategy1 implements PlayBehaviour {
-    public ArrayList<Meld> determineMove(ArrayList<Meld> workspace, Hand hand, boolean initialMove) {
-        if (initialMove) {
-            return determineInitialMove(workspace, hand);
-        }
-        return determineRegularMove(workspace, hand);
-    }
-    
+public class Strategy1 extends PlayBehaviour {
     // Plays as many melds as it can using only the tiles in its hand and only if the total of the melds is >= 30
-    public ArrayList<Meld> determineInitialMove(ArrayList<Meld> workspace, Hand hand) {
-        // First attempt to find largest meld that is >= 30 points
-        ArrayList<Meld> melds1 = new ArrayList<>();
-        for (int i = 0; i < hand.getSize(); i++) {
-            // For each tile in hand, add it to a new meld
-            ArrayList<Tile> tiles = new ArrayList<>();
-            tiles.add(hand.getTile(i));
-            Meld meld = new Meld();
-            meld.addTile(tiles);
-            melds1.add(meld);
-            
-            // For each tile in hand, attempt to add it to every preceding meld
-            for (Meld tempMeld : melds1) {
-                tempMeld.addTile(tiles);
-            }      
+    public ArrayList<Meld> determineInitialMove(Hand hand) {
+        // Return largest meld >= 30, if it exists
+        ArrayList<Meld> melds = createMeldsFromHand(hand);
+        Meld largestMeldOver30 = getLargestMeldOver30(melds);
+        if (largestMeldOver30 != null) {
+            this.workspace.add(largestMeldOver30);
+            return this.workspace;
         }
         
-        // Filter melds totalling >= 30
-        ArrayList<Meld> filteredMelds = new ArrayList<>();
-        for (Meld tempMeld : melds1) {
-            if (tempMeld.getValue() >= 30) {
-                filteredMelds.add(tempMeld);
-            }
-        }
-
-        // If there are melds with 30+ points, add largest to workspace and return
-        if (filteredMelds.size() > 0) {
-            Meld largestMeld = filteredMelds.get(0);
-            for (Meld tempMeld : filteredMelds) {
-                if (tempMeld.getValue() > largestMeld.getValue()) {
-                    largestMeld = tempMeld;
-                }
-            }
-            workspace.add(largestMeld);
-            return workspace;
-        }
-        
-        // Otherwise, add as many melds as possible to workspace such that total points >= 30
+        // Otherwise, adds as many melds as possible to workspace such that total points >= 30
         int totalTileValue = 0;
         boolean hasMelds = true;
         while (hasMelds && hand.getSize() != 0) {
-            ArrayList<Meld> melds2 = new ArrayList<>();
-            for (int i = 0; i < hand.getSize(); i++) {
-                ArrayList<Tile> tiles = new ArrayList<>();
-                tiles.add(hand.getTile(i));
-                Meld meld = new Meld();
-                meld.addTile(tiles);
-                melds2.add(meld);
-                
-                for (Meld tempMeld : melds2) {
-                    tempMeld.addTile(tiles);
-                }
-            }
-
-            // Find meld with greatest value
-            Meld greatestMeld = melds2.get(0);
-            for (Meld tempMeld : melds2) {
-                if ((tempMeld.isValidMeld() && !greatestMeld.isValidMeld()) || 
-                    (tempMeld.getValue() > greatestMeld.getValue() && tempMeld.isValidMeld())) {
-                    greatestMeld = tempMeld;
-                }
-            }
-           
-            if (greatestMeld.isValidMeld()) {
-                workspace.add(greatestMeld);
+            ArrayList<Meld> currentMelds = createMeldsFromHand(hand);
+            Meld greatestMeld = getGreatestMeld(currentMelds);
+            if (greatestMeld != null) {
+                this.workspace.add(greatestMeld);
                 totalTileValue += greatestMeld.getValue();
                 for (int j = 0; j < greatestMeld.getSize(); j++) {
                     hand.remove(greatestMeld.getTile(j));
@@ -86,73 +31,52 @@ public class Strategy1 implements PlayBehaviour {
         }
         
         if (totalTileValue >= 30) {
-            return workspace;
+            return this.workspace;
         }
         return null;
     }
     
     // Plays all the tiles it can using its hand and the table
-    public ArrayList<Meld> determineRegularMove(ArrayList<Meld> workspace, Hand hand) {
-        boolean canPlayUsingHand = true;
-        boolean canPlayUsingTable = true;
+    public ArrayList<Meld> determineRegularMove(Hand hand) {
+        boolean hasMelds = true;
         boolean workspaceChanged = false;
-        while (canPlayUsingHand && hand.getSize() > 0) {
-            ArrayList<Meld> tempMelds = new ArrayList<>();
-            for (int i = 0; i < hand.getSize(); i++)  {
-                // For each time in hand, add it to a new meld
-                ArrayList<Tile> tiles = new ArrayList<>();
-                tiles.add(hand.getTile(i));
-                Meld meld = new Meld();
-                meld.addTile(tiles);
-                tempMelds.add(meld);
-                
-                // For each tile in hand, attempt to add it to every preceding meld
-                for (Meld tempMeld : tempMelds) {
-                    tempMeld.addTile(tiles);
-                }
-            }
-            
-            // Once all tiles have been added to melds, find the largest meld
-            Meld largestMeld = tempMelds.get(0);
-            for (Meld tempMeld : tempMelds) {
-                if (tempMeld.getSize() > largestMeld.getSize()) {
-                    largestMeld = tempMeld;
-                }
-            }
-            
-            if (largestMeld.isValidMeld()) {
-                workspace.add(largestMeld);
+        // First tries to add new melds using tiles in hand
+        while (hasMelds && hand.getSize() > 0) {
+            ArrayList<Meld> melds = createMeldsFromHand(hand);
+            Meld largestMeld = getLargestMeld(melds);
+            if (largestMeld != null) {
+                this.workspace.add(largestMeld);
                 workspaceChanged = true;
                 for (int j = 0; j < largestMeld.getSize(); j++) {
                     hand.remove(largestMeld.getTile(j));
                 }
             } else {
-                canPlayUsingHand = false;
+                hasMelds = false;
             }
         }
         
-        while (canPlayUsingTable && hand.getSize() > 0) {
-            // Attempt to add each tile in hand to each meld, removing from hand if added
-            int tilesAdded = 0;
+        // Then tries to add each tiles to every existing melds
+        hasMelds = true;
+        while (hasMelds && hand.getSize() > 0) {
+            boolean tileAdded = false;
             for (int i = 0; i < hand.getSize(); i++) {
-                for (Meld tempMeld : workspace) {
-                    ArrayList<Tile> tiles = new ArrayList<>();
-                    tiles.add(hand.getTile(i));
+                ArrayList<Tile> tiles = new ArrayList<>();
+                tiles.add(hand.getTile(i));
+                for (Meld tempMeld : this.workspace) {
                     if (tempMeld.addTile(tiles)) {
-                        tilesAdded++;
+                        tileAdded = true;
                         workspaceChanged = true;
                         hand.remove(i);
                         break;
                     }
                 }
             }
-            if (tilesAdded == 0) {
-                canPlayUsingTable = false;
+            if (!tileAdded) {
+                hasMelds = false;
             }
         }
-        
         if (workspaceChanged == true) {
-            return workspace;
+            return this.workspace;
         }
         return null;
     }
