4aadc618a3fe9dbc7c15714418732c4b1e6f848f
Randy Taylor <tehgecKozzz@gmail.com>
2018-10-28 16:05:04 -0400

    [test:Strategy2Test] Fix incorrection assertion in testDetermineInitialMove1

--- src/test/java/core/Strategy2Test.java
+++ src/test/java/core/Strategy2Test.java
@@ -17,18 +17,18 @@ public class Strategy2Test extends TestCase {
         hand.add(new Tile(Colour.GREEN, 9));
         hand.add(new Tile(Colour.GREEN, 10));
         strategy2.setWorkspace(workspace);
-        
+
         assertTrue(strategy2.getInitialMove());
         assertEquals(null, strategy2.determineMove(hand));
-        assertFalse(strategy2.getInitialMove());
+        assertTrue(strategy2.getInitialMove());
     }
-    
+
     // Test first move with one meld >= 30 (R10, B10, G10, O10)
     public void testDetermineInitialMove2() {
         PlayBehaviour strategy2 = new Strategy2();
         ArrayList<Meld> workspace = new ArrayList<>();
         Hand hand = new Hand();
-        
+
         ArrayList<Tile> tiles1 = new ArrayList<>();
         tiles1.add(new Tile(Colour.RED, 1));
         tiles1.add(new Tile(Colour.RED, 2));
@@ -36,7 +36,7 @@ public class Strategy2Test extends TestCase {
         Meld meld1 = new Meld();
         meld1.addTile(tiles1);
         workspace.add(meld1);
-        
+
         ArrayList<Tile> tiles2 = new ArrayList<>();
         tiles2.add(new Tile(Colour.RED, 10));
         tiles2.add(new Tile(Colour.ORANGE, 10));
@@ -45,7 +45,7 @@ public class Strategy2Test extends TestCase {
         Meld meld2 = new Meld();
         meld2.addTile(tiles2);
         workspace.add(meld2);
-        
+
         ArrayList<Tile> tiles3 = new ArrayList<>();
         tiles3.add(new Tile(Colour.BLUE, 5));
         tiles3.add(new Tile(Colour.BLUE, 6));
@@ -54,9 +54,9 @@ public class Strategy2Test extends TestCase {
         Meld meld3 = new Meld();
         meld3.addTile(tiles3);
         workspace.add(meld3);
-        
+
         assertEquals("[{R1,R2,R3}, {R10,O10,G10,B10}, {B5,B6,B7,B8}]", workspace.toString());
-      
+
         hand.add(new Tile(Colour.GREEN, 10));
         hand.add(new Tile(Colour.ORANGE, 10));
         hand.add(new Tile(Colour.BLUE, 10));
@@ -67,19 +67,19 @@ public class Strategy2Test extends TestCase {
         hand.add(new Tile(Colour.RED, 5));
         hand.add(new Tile(Colour.RED, 4));
         strategy2.setWorkspace(workspace);
-        
+
         assertTrue(strategy2.getInitialMove());
         assertEquals("[{R1,R2,R3}, {R10,O10,G10,B10}, {B5,B6,B7,B8}, {R10,B10,G10,O10}]", strategy2.determineMove(hand).toString());
         assertFalse(strategy2.getInitialMove());
     }
-    
+
     // Test first move such that multiple melds total >= 30, should use as few melds as possible
     // Uses example given by prof
     public void testDetermineInitialMove3() {
         PlayBehaviour strategy2 = new Strategy2();
         ArrayList<Meld> workspace = new ArrayList<>();
         Hand hand = new Hand();
-        
+
         ArrayList<Tile> tiles1 = new ArrayList<>();
         tiles1.add(new Tile(Colour.RED, 1));
         tiles1.add(new Tile(Colour.RED, 2));
@@ -87,7 +87,7 @@ public class Strategy2Test extends TestCase {
         Meld meld1 = new Meld();
         meld1.addTile(tiles1);
         workspace.add(meld1);
-        
+
         ArrayList<Tile> tiles2 = new ArrayList<>();
         tiles2.add(new Tile(Colour.RED, 10));
         tiles2.add(new Tile(Colour.ORANGE, 10));
@@ -96,7 +96,7 @@ public class Strategy2Test extends TestCase {
         Meld meld2 = new Meld();
         meld2.addTile(tiles2);
         workspace.add(meld2);
-        
+
         ArrayList<Tile> tiles3 = new ArrayList<>();
         tiles3.add(new Tile(Colour.BLUE, 5));
         tiles3.add(new Tile(Colour.BLUE, 6));
@@ -105,9 +105,9 @@ public class Strategy2Test extends TestCase {
         Meld meld3 = new Meld();
         meld3.addTile(tiles3);
         workspace.add(meld3);
-        
+
         assertEquals("[{R1,R2,R3}, {R10,O10,G10,B10}, {B5,B6,B7,B8}]", workspace.toString());
-        
+
         hand.add(new Tile(Colour.RED, 3));
         hand.add(new Tile(Colour.RED, 4));
         hand.add(new Tile(Colour.RED, 5));
@@ -118,18 +118,18 @@ public class Strategy2Test extends TestCase {
         hand.add(new Tile(Colour.ORANGE, 3));
         hand.add(new Tile(Colour.ORANGE, 4));
         strategy2.setWorkspace(workspace);
-        
+
         assertTrue(strategy2.getInitialMove());
         assertEquals("[{R1,R2,R3}, {R10,O10,G10,B10}, {B5,B6,B7,B8}, {G5,G6,G7}, {R3,R4,R5}]", strategy2.determineMove(hand).toString());
         assertFalse(strategy2.getInitialMove());
     }
-    
+
     // Test first move where no combination of melds totals >= 30
     public void testDetermineInitialMove4( ) {
         PlayBehaviour strategy2 = new Strategy2();
         ArrayList<Meld> workspace = new ArrayList<>();
         Hand hand = new Hand();
-        
+
         ArrayList<Tile> tiles1 = new ArrayList<>();
         tiles1.add(new Tile(Colour.RED, 1));
         tiles1.add(new Tile(Colour.RED, 2));
@@ -137,7 +137,7 @@ public class Strategy2Test extends TestCase {
         Meld meld1 = new Meld();
         meld1.addTile(tiles1);
         workspace.add(meld1);
-        
+
         ArrayList<Tile> tiles2 = new ArrayList<>();
         tiles2.add(new Tile(Colour.RED, 10));
         tiles2.add(new Tile(Colour.ORANGE, 10));
@@ -146,7 +146,7 @@ public class Strategy2Test extends TestCase {
         Meld meld2 = new Meld();
         meld2.addTile(tiles2);
         workspace.add(meld2);
-        
+
         ArrayList<Tile> tiles3 = new ArrayList<>();
         tiles3.add(new Tile(Colour.BLUE, 5));
         tiles3.add(new Tile(Colour.BLUE, 6));
@@ -155,9 +155,9 @@ public class Strategy2Test extends TestCase {
         Meld meld3 = new Meld();
         meld3.addTile(tiles3);
         workspace.add(meld3);
-        
+
         assertEquals("[{R1,R2,R3}, {R10,O10,G10,B10}, {B5,B6,B7,B8}]", workspace.toString());
-        
+
         hand.add(new Tile(Colour.GREEN, 1));
         hand.add(new Tile(Colour.GREEN, 2));
         hand.add(new Tile(Colour.GREEN, 3));
@@ -173,7 +173,7 @@ public class Strategy2Test extends TestCase {
         hand.add(new Tile(Colour.RED, 12));
         hand.add(new Tile(Colour.BLUE, 7));
         strategy2.setWorkspace(workspace);
-        
+
         assertTrue(strategy2.getInitialMove());
         assertEquals(null, strategy2.determineMove(hand));
         assertTrue(strategy2.getInitialMove());
