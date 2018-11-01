324541d0bc032f04086146b5d76f40cd729b8b0d
Altin R <altin.rex@gmail.com>
2018-11-01 02:15:17 -0400

    [refactor:Player1:Player2:Player3:PlayerHuman] Refactor to implement abstract method play() from Player

--- src/main/java/core/Player2.java
+++ src/main/java/core/Player2.java
@@ -1,8 +1,24 @@
 package core;
 
+import java.util.ArrayList;
+
 public class Player2 extends Player {
     public Player2() {
         this.playBehaviour = new Strategy2();
+    }
+
+    protected ArrayList<Meld> play() {
+        ArrayList<Meld> workspace = new ArrayList<>();
+        if (this.initialMove && this.getWorkspace().size() > 0) {
+            workspace = this.playBehaviour.determineInitialMove(hand);
+        } else {
+            workspace = this.playBehaviour.determineRegularMove(hand);
+        }
+        if (workspace != null) {
+            this.initialMove = false;
+            this.notifyObservers();
+        }
+        return workspace;
     }
 
     public String toString() {
--- src/main/java/core/Player3.java
+++ src/main/java/core/Player3.java
@@ -1,8 +1,28 @@
 package core;
 
+import java.util.ArrayList;
+
 public class Player3 extends Player {
     public Player3() {
-        this.playBehaviour = new Strategy3();
+        this.playBehaviour = new Strategy2();
+    }
+
+    protected ArrayList<Meld> play() {
+        ArrayList<Meld> workspace = new ArrayList<>();
+        if (this.initialMove) {
+            workspace = this.playBehaviour.determineInitialMove(hand);
+        } else if (this.hand.getSize() - this.playBehaviour.getLowestHandCount() >= 3) {
+            this.playBehaviour = new Strategy1();
+            workspace = this.playBehaviour.determineRegularMove(hand);
+        } else {
+            this.playBehaviour = new Strategy2();
+            workspace = this.playBehaviour.determineRegularMove(hand);
+        }  
+        if (workspace != null) {
+            this.initialMove = false;
+            this.notifyObservers();
+        }
+        return workspace;
     }
 
     public String toString() {
