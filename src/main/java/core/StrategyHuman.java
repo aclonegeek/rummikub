c3cddcaad0ad530f3d91bbd75333d227cff12339
Altin R <altin.rex@gmail.com>
2018-11-01 02:20:46 -0400

    [refactor:Strategy2:StrategyHuman] Refactor to implement abstract methods from PlayBehaviour

--- src/main/java/core/StrategyHuman.java
+++ src/main/java/core/StrategyHuman.java
@@ -1,5 +1,14 @@
 package core;
 
+import java.util.ArrayList;
+
 public class StrategyHuman extends PlayBehaviour {
+    
+    ArrayList<Meld> determineInitialMove(Hand hand) {
+        return null;
+    }
+    ArrayList<Meld> determineRegularMove(Hand hand) {
+        return null;
+    }
  
 }
