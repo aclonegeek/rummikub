e2ece31da165ee0d8a0fa673dbe1bacb96a15801
LayneKM <laynemikan@gmail.com>
2018-11-03 15:01:02 -0400

    [refactor:Strategy2Test] Update test cases to use string ctors, added some new test cases

--- src/test/java/core/Strategy2Test.java
+++ src/test/java/core/Strategy2Test.java
@@ -6,274 +6,131 @@ import core.Globals.Colour;
 import junit.framework.TestCase;
 
 public class Strategy2Test extends TestCase {
-    // Test first move with empty table (should not make a move)
-    public void testDetermineInitialMove1() {
-        Player p2 = new Player2();
-        ArrayList<Meld> workspace = new ArrayList<>();
-
-        p2.add(new Tile(Colour.GREEN, 6));
-        p2.add(new Tile(Colour.GREEN, 7));
-        p2.add(new Tile(Colour.GREEN, 8));
-        p2.add(new Tile(Colour.GREEN, 9));
-        p2.add(new Tile(Colour.GREEN, 10));
-        p2.getPlayBehaviour().setWorkspace(workspace);
-
-        assertTrue(p2.getInitialMove());
-        assertEquals(null, p2.play());
-        assertTrue(p2.getInitialMove());
-    }
-
     // Test first move with one meld >= 30
     public void testDetermineInitialMove2() {
-        Player p2 = new Player2();
+        Strategy2 strategy2 = new Strategy2();
         ArrayList<Meld> workspace = new ArrayList<>();
-
-        ArrayList<Tile> tiles1 = new ArrayList<>();
-        tiles1.add(new Tile(Colour.RED, 1));
-        tiles1.add(new Tile(Colour.RED, 2));
-        tiles1.add(new Tile(Colour.RED, 3));
-        Meld meld1 = new Meld();
-        meld1.addTile(tiles1);
+        Meld meld1 = new Meld("R1,R2,R3");
+        Meld meld2 = new Meld("R10,O10,G10,B10");
+        Meld meld3 = new Meld("B5,B6,B7,B8");
         workspace.add(meld1);
-
-        ArrayList<Tile> tiles2 = new ArrayList<>();
-        tiles2.add(new Tile(Colour.RED, 10));
-        tiles2.add(new Tile(Colour.ORANGE, 10));
-        tiles2.add(new Tile(Colour.GREEN, 10));
-        tiles2.add(new Tile(Colour.BLUE, 10));
-        Meld meld2 = new Meld();
-        meld2.addTile(tiles2);
         workspace.add(meld2);
-
-        ArrayList<Tile> tiles3 = new ArrayList<>();
-        tiles3.add(new Tile(Colour.BLUE, 5));
-        tiles3.add(new Tile(Colour.BLUE, 6));
-        tiles3.add(new Tile(Colour.BLUE, 7));
-        tiles3.add(new Tile(Colour.BLUE, 8));
-        Meld meld3 = new Meld();
-        meld3.addTile(tiles3);
         workspace.add(meld3);
+        strategy2.setWorkspace(workspace);
+        assertEquals("[{R1,R2,R3}, {R10,O10,G10,B10}, {B5,B6,B7,B8}]", strategy2.getWorkspace().toString());
 
-        assertEquals("[{R1,R2,R3}, {R10,O10,G10,B10}, {B5,B6,B7,B8}]", workspace.toString());
-
-        p2.add(new Tile(Colour.GREEN, 10));
-        p2.add(new Tile(Colour.ORANGE, 10));
-        p2.add(new Tile(Colour.BLUE, 10));
-        p2.add(new Tile(Colour.RED, 10));
-        p2.add(new Tile(Colour.RED, 8));
-        p2.add(new Tile(Colour.RED, 7));
-        p2.add(new Tile(Colour.RED, 6));
-        p2.add(new Tile(Colour.RED, 5));
-        p2.add(new Tile(Colour.RED, 4));
-        p2.getPlayBehaviour().setWorkspace(workspace);
-        assertEquals("[{R1,R2,R3}, {R10,O10,G10,B10}, {B5,B6,B7,B8}]", workspace.toString());
-
-        assertTrue(p2.getInitialMove());
-        assertEquals("[{R1,R2,R3}, {R10,O10,G10,B10}, {B5,B6,B7,B8}, {R4,R5,R6,R7,R8}]", p2.play().toString());
-        assertFalse(p2.getInitialMove());
+        Hand hand = new Hand("G10,O10,B10,R10,R8,R7,R6,R5,R4");
+        assertEquals("[{R1,R2,R3}, {R10,O10,G10,B10}, {B5,B6,B7,B8}, {R4,R5,R6,R7,R8}]", strategy2.determineInitialMove(hand).toString());
     }
 
-    // Test first move such that multiple melds total >= 30, should use as few melds
-    // as possible
-    // Uses example given by prof
+    // Test first move such that multiple melds total >= 30, should use as few melds as possible
     public void testDetermineInitialMove3() {
-        Player p2 = new Player2();
+        Strategy2 strategy2 = new Strategy2();
         ArrayList<Meld> workspace = new ArrayList<>();
-
-        ArrayList<Tile> tiles1 = new ArrayList<>();
-        tiles1.add(new Tile(Colour.RED, 1));
-        tiles1.add(new Tile(Colour.RED, 2));
-        tiles1.add(new Tile(Colour.RED, 3));
-        Meld meld1 = new Meld();
-        meld1.addTile(tiles1);
+        Meld meld1 = new Meld("R1,R2,R3");
+        Meld meld2 = new Meld("R10,O10,G10,B10");
+        Meld meld3 = new Meld("B5,B6,B7,B8");
         workspace.add(meld1);
-
-        ArrayList<Tile> tiles2 = new ArrayList<>();
-        tiles2.add(new Tile(Colour.RED, 10));
-        tiles2.add(new Tile(Colour.ORANGE, 10));
-        tiles2.add(new Tile(Colour.GREEN, 10));
-        tiles2.add(new Tile(Colour.BLUE, 10));
-        Meld meld2 = new Meld();
-        meld2.addTile(tiles2);
         workspace.add(meld2);
-
-        ArrayList<Tile> tiles3 = new ArrayList<>();
-        tiles3.add(new Tile(Colour.BLUE, 5));
-        tiles3.add(new Tile(Colour.BLUE, 6));
-        tiles3.add(new Tile(Colour.BLUE, 7));
-        tiles3.add(new Tile(Colour.BLUE, 8));
-        Meld meld3 = new Meld();
-        meld3.addTile(tiles3);
         workspace.add(meld3);
+        strategy2.setWorkspace(workspace);
+        assertEquals("[{R1,R2,R3}, {R10,O10,G10,B10}, {B5,B6,B7,B8}]", strategy2.getWorkspace().toString());
 
-        assertEquals("[{R1,R2,R3}, {R10,O10,G10,B10}, {B5,B6,B7,B8}]", workspace.toString());
+        Hand hand = new Hand("R3,R4,R5,G5,G6,G7,O2,O3,O4,B1");
+        assertEquals("[{R1,R2,R3}, {R10,O10,G10,B10}, {B5,B6,B7,B8}, {R3,R4,R5}, {G5,G6,G7}]", strategy2.determineInitialMove(hand).toString());
+    }
+    
+    // Test case where melds are being constructed but hand size reaches 0 (needed for full coverage)
+    public void testDetermineInitialMove4() {
+        Strategy2 strategy2 = new Strategy2();
+        ArrayList<Meld> workspace = new ArrayList<>();
+        Meld meld1 = new Meld("R1,R2,R3");
+        Meld meld2 = new Meld("R10,O10,G10,B10");
+        Meld meld3 = new Meld("B5,B6,B7,B8");
+        workspace.add(meld1);
+        workspace.add(meld2);
+        workspace.add(meld3);
+        strategy2.setWorkspace(workspace);
+        assertEquals("[{R1,R2,R3}, {R10,O10,G10,B10}, {B5,B6,B7,B8}]", strategy2.getWorkspace().toString());
 
-        p2.add(new Tile(Colour.RED, 3));
-        p2.add(new Tile(Colour.RED, 4));
-        p2.add(new Tile(Colour.RED, 5));
-        p2.add(new Tile(Colour.GREEN, 5));
-        p2.add(new Tile(Colour.GREEN, 6));
-        p2.add(new Tile(Colour.GREEN, 7));
-        p2.add(new Tile(Colour.ORANGE, 2));
-        p2.add(new Tile(Colour.ORANGE, 3));
-        p2.add(new Tile(Colour.ORANGE, 4));
-        p2.getPlayBehaviour().setWorkspace(workspace);
-
-        assertTrue(p2.getInitialMove());
-        assertEquals("[{R1,R2,R3}, {R10,O10,G10,B10}, {B5,B6,B7,B8}, {R3,R4,R5}, {G5,G6,G7}]", p2.play().toString());
-        assertFalse(p2.getInitialMove());
+        Hand hand = new Hand("R3,R4,R5,G1,G2,G3");
+        assertEquals(null, strategy2.determineInitialMove(hand));
     }
 
     // Test first move where no combination of melds totals >= 30
-    public void testDetermineInitialMove4() {
-        Player p2 = new Player2();
+    public void testDetermineInitialMove5() {
+        Strategy2 strategy2 = new Strategy2();
         ArrayList<Meld> workspace = new ArrayList<>();
-
-        ArrayList<Tile> tiles1 = new ArrayList<>();
-        tiles1.add(new Tile(Colour.RED, 1));
-        tiles1.add(new Tile(Colour.RED, 2));
-        tiles1.add(new Tile(Colour.RED, 3));
-        Meld meld1 = new Meld();
-        meld1.addTile(tiles1);
+        Meld meld1 = new Meld("R1,R2,R3");
+        Meld meld2 = new Meld("R10,O10,G10,B10");
+        Meld meld3 = new Meld("B5,B6,B7,B8");
         workspace.add(meld1);
-
-        ArrayList<Tile> tiles2 = new ArrayList<>();
-        tiles2.add(new Tile(Colour.RED, 10));
-        tiles2.add(new Tile(Colour.ORANGE, 10));
-        tiles2.add(new Tile(Colour.GREEN, 10));
-        tiles2.add(new Tile(Colour.BLUE, 10));
-        Meld meld2 = new Meld();
-        meld2.addTile(tiles2);
         workspace.add(meld2);
-
-        ArrayList<Tile> tiles3 = new ArrayList<>();
-        tiles3.add(new Tile(Colour.BLUE, 5));
-        tiles3.add(new Tile(Colour.BLUE, 6));
-        tiles3.add(new Tile(Colour.BLUE, 7));
-        tiles3.add(new Tile(Colour.BLUE, 8));
-        Meld meld3 = new Meld();
-        meld3.addTile(tiles3);
         workspace.add(meld3);
+        strategy2.setWorkspace(workspace);
+        assertEquals("[{R1,R2,R3}, {R10,O10,G10,B10}, {B5,B6,B7,B8}]", strategy2.getWorkspace().toString());
 
-        assertEquals("[{R1,R2,R3}, {R10,O10,G10,B10}, {B5,B6,B7,B8}]", workspace.toString());
-
-        p2.add(new Tile(Colour.GREEN, 1));
-        p2.add(new Tile(Colour.GREEN, 2));
-        p2.add(new Tile(Colour.GREEN, 3));
-        p2.add(new Tile(Colour.GREEN, 4));
-        p2.add(new Tile(Colour.GREEN, 8));
-        p2.add(new Tile(Colour.ORANGE, 10));
-        p2.add(new Tile(Colour.BLUE, 2));
-        p2.add(new Tile(Colour.RED, 9));
-        p2.add(new Tile(Colour.RED, 4));
-        p2.add(new Tile(Colour.GREEN, 5));
-        p2.add(new Tile(Colour.ORANGE, 2));
-        p2.add(new Tile(Colour.RED, 11));
-        p2.add(new Tile(Colour.RED, 12));
-        p2.add(new Tile(Colour.BLUE, 7));
-        p2.getPlayBehaviour().setWorkspace(workspace);
-
-        assertTrue(p2.getInitialMove());
-        assertEquals(null, p2.play());
-        assertTrue(p2.getInitialMove());
+        Hand hand = new Hand("G1,G2,G3,G4,G8,O10,B2,R9,R4,G5,O2,R11,R12,B7");
+        assertEquals(null, strategy2.determineInitialMove(hand));
     }
     
     // Plays all its tiles and wins
     public void testDetermineRegularMove1() {
-        Player p2 = new Player2();
+        Strategy2 strategy2 = new Strategy2();
         ArrayList<Meld> workspace = new ArrayList<>();
-
-        ArrayList<Tile> tiles1 = new ArrayList<>();
-        tiles1.add(new Tile(Colour.RED, 1));
-        tiles1.add(new Tile(Colour.RED, 2));
-        tiles1.add(new Tile(Colour.RED, 3));
-        Meld meld1 = new Meld();
-        meld1.addTile(tiles1);
+        Meld meld1 = new Meld("R1,R2,R3");
         workspace.add(meld1);
-
-        assertEquals("[{R1,R2,R3}]", workspace.toString());
+        strategy2.setWorkspace(workspace);
+        assertEquals("[{R1,R2,R3}]", strategy2.getWorkspace().toString());
         
-        p2.setInitialMove(false);
-        p2.add(new Tile(Colour.GREEN, 1));
-        p2.add(new Tile(Colour.GREEN, 2));
-        p2.add(new Tile(Colour.GREEN, 3));
-        p2.add(new Tile(Colour.GREEN, 4));
-        p2.getPlayBehaviour().setWorkspace(workspace);
-        
-        p2.setInitialMove(false);
-        assertEquals("[{R1,R2,R3}, {G1,G2,G3,G4}]", p2.play().toString());
-        assertEquals(0, p2.getHandSize());
+        Hand hand = new Hand("G1,G2,G3,G4");
+        assertEquals("[{R1,R2,R3}, {G1,G2,G3,G4}]", strategy2.determineRegularMove(hand).toString());
+        assertEquals(0, hand.getSize());
     }
     
-    // Can't place full meld on table, nothing to do with G8
+    // Doesn't play because it can't win on this turn (can't create new meld G1,G2,G3,G4)
     public void testDetermineRegularMove2() {
-        Player p2 = new Player2();
+        Strategy2 strategy2 = new Strategy2();
         ArrayList<Meld> workspace = new ArrayList<>();
-
-        ArrayList<Tile> tiles1 = new ArrayList<>();
-        tiles1.add(new Tile(Colour.RED, 1));
-        tiles1.add(new Tile(Colour.RED, 2));
-        tiles1.add(new Tile(Colour.RED, 3));
-        Meld meld1 = new Meld();
-        meld1.addTile(tiles1);
+        Meld meld1 = new Meld("R1,R2,R3");
         workspace.add(meld1);
-
-        assertEquals("[{R1,R2,R3}]", workspace.toString());
+        strategy2.setWorkspace(workspace);
+        assertEquals("[{R1,R2,R3}]", strategy2.getWorkspace().toString());
         
-        p2.add(new Tile(Colour.GREEN, 1));
-        p2.add(new Tile(Colour.GREEN, 2));
-        p2.add(new Tile(Colour.GREEN, 3));
-        p2.add(new Tile(Colour.GREEN, 4));
-        p2.add(new Tile(Colour.GREEN, 8));
-        p2.getPlayBehaviour().setWorkspace(workspace);
-        
-        p2.setInitialMove(false);
-        assertEquals(null, p2.play());
-        assertEquals(1, p2.getHandSize());
+        Hand hand = new Hand("G1,G2,G3,G4,G8");
+        assertEquals(null, strategy2.determineRegularMove(hand));
+        assertEquals(5, hand.getSize());
     }
     
-    // Places tile to existing meld
-    public void testDetermineRegularMove3() {
-        Player p2 = new Player2();
-        ArrayList<Meld> workspace = new ArrayList<>();
-
-        ArrayList<Tile> tiles1 = new ArrayList<>();
-        tiles1.add(new Tile(Colour.RED, 1));
-        tiles1.add(new Tile(Colour.RED, 2));
-        tiles1.add(new Tile(Colour.RED, 3));
-        Meld meld1 = new Meld();
-        meld1.addTile(tiles1);
-        workspace.add(meld1);
-
-        assertEquals("[{R1,R2,R3}]", workspace.toString());
-        
-        p2.add(new Tile(Colour.RED, 4));
-        p2.getPlayBehaviour().setWorkspace(workspace);
-        
-        p2.setInitialMove(false);
-        assertEquals("[{R1,R2,R3,R4}]", p2.play().toString());
-        assertEquals(0, p2.getHandSize());
-    }
-    
-    // Places tile to existing meld
+    // Plays melds using tiles on table with no tiles left over
     public void testDetermineRegularMove4() {
-        Player p2 = new Player2();
+        Strategy2 strategy2 = new Strategy2();
         ArrayList<Meld> workspace = new ArrayList<>();
 
         workspace.add(new Meld("R1,R2,R3,R4,R5,R6,R7"));
         workspace.add(new Meld("G2,G3,G4,G5"));
         workspace.add(new Meld("G3,R3,B3,O3"));
-        p2.getPlayBehaviour().setWorkspace(workspace);
-        System.out.println(workspace);
+        strategy2.setWorkspace(workspace);
+        assertEquals("[{R1,R2,R3,R4,R5,R6,R7}, {G2,G3,G4,G5}, {G3,R3,B3,O3}]", strategy2.getWorkspace().toString());
         
-        p2.add(new Tile(Colour.RED, 2));
-        p2.add(new Tile(Colour.RED, 3));
+        Hand hand = new Hand("R2,R3,R8,G1");
+        assertEquals("[{R2,R3,R4,R5,R6,R7,R8}, {G3,G4,G5}, {R3,B3,O3}, {R1,R2,R3}, {G1,G2,G3}]", strategy2.determineRegularMove(hand).toString());
+        assertEquals(0, hand.getSize());
+    }
+    
+    // Plays melds using tiles on table with tiles left over
+    public void testDetermineRegularMove5() {
+        Strategy2 strategy2 = new Strategy2();
+        ArrayList<Meld> workspace = new ArrayList<>();
+
+        workspace.add(new Meld("R1,R2,R3,R4,R5,R6,R7"));
+        workspace.add(new Meld("G2,G3,G4,G5"));
+        workspace.add(new Meld("G3,R3,B3,O3"));
+        strategy2.setWorkspace(workspace);
+        assertEquals("[{R1,R2,R3,R4,R5,R6,R7}, {G2,G3,G4,G5}, {G3,R3,B3,O3}]", strategy2.getWorkspace().toString());
         
-        // Bug when adding R8
-//        p2.add(new Tile(Colour.RED, 8));
-        p2.add(new Tile(Colour.GREEN, 1));
-        
-        p2.setInitialMove(false);
-        System.out.println(p2.play().toString());
+        Hand hand = new Hand("R2,R3,R8,G1,O7");
+        assertEquals("[{R2,R3,R4,R5,R6,R7,R8}, {G3,G4,G5}, {R3,B3,O3}, {R1,R2,R3}, {G1,G2,G3}]", strategy2.determineRegularMove(hand).toString());
+        assertEquals(1, hand.getSize());
     }
 }
