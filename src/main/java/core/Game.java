e6181fec8ae833ff48203a319bf6a883811ffe32
LayneKM <laynemikan@gmail.com>
2018-11-16 14:54:20 -0500

    [code:Game] Implement ability to choose 2-4 players and their strategies, refactored some game behaviour to support this, tableState now passed as parameter to play

--- src/main/java/core/Game.java
+++ src/main/java/core/Game.java
@@ -1,7 +1,9 @@
 package core;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Scanner;
+
+import core.Globals.PlayerType;
 
 public class Game {
     protected Stock stock;
@@ -10,8 +12,7 @@ public class Game {
 
     // Testing related attributes
     private boolean testing = false;
-    private boolean fourthPlayer = false;
-    private ArrayList<Hand> riggedHands;
+    private ArrayList<Player> riggedPlayers;
     private Stock riggedStock;
 
     public Game() {}
@@ -20,79 +21,54 @@ public class Game {
         this.testing = testing;
     }
 
-    public Game(boolean testing, boolean fourthPlayer) {
-        this.testing = testing;
-        this.fourthPlayer = fourthPlayer;
-    }
-
     public void start() {
         this.playWithConsole();
     }
 
-    public void rig(ArrayList<Hand> riggedHands, Stock riggedStock) {
+    public void rig(ArrayList<Player> riggedPlayers, Stock riggedStock) {
         this.riggedStock = riggedStock;
-        this.riggedHands = riggedHands;
+        this.riggedPlayers = riggedPlayers;
     }
 
     protected void setup() {
         if (testing) {
             this.stock = this.riggedStock;
+            this.players = this.riggedPlayers;
         } else {
             this.stock = new Stock();
             this.stock.populate();
             this.stock.shuffle();
-        }
-        this.table = new Table();
-
-        Player playerHuman = new PlayerHuman("PlayerHuman");
-        Player player1     = new Player1("Player1");
-        Player player2     = new Player2("Player2");
-        Player player3     = new Player3("Player3");
-        Player player4     = null;
-        this.players = new ArrayList<>(Arrays.asList(playerHuman, player1, player2, player3));
-        if (this.fourthPlayer) {
-            player4 = new Player4("Player4");
-            this.players.add(player4);
-        }
-
-        // Register each player's PlayBehaviour as an observer of the table and give each player 14 tiles.
-        int index = 0;
-        for (Player player : this.players) {
-            this.table.registerObserver(player.getPlayBehaviour());
-            if (testing) {
-                Hand hand = this.riggedHands.get(index);
-                for (int i = 0; i < hand.getSize(); i++) {
-                    player.add(hand.getTile(i));
-                }
-                index++;
-            } else {
+            this.createPlayers();
+            for (Player player : this.players) {
                 for (int i = 0; i < 14; i++) {
                     player.add(stock.draw());
                 }
             }
         }
+        
+        this.table = new Table();
 
         // Display each player's initial hand
         for (Player player : this.players) {
             System.out.println(player.toString());
         }
 
-        // Register player 3's subjects.
-        playerHuman.registerObserver(player3);
-        player1.registerObserver(player3);
-        player2.registerObserver(player3);
-
-        if (this.fourthPlayer) {
-            playerHuman.registerObserver(player4);
-            player1.registerObserver(player4);
-            player2.registerObserver(player4);
+        // Register each player that uses Strategy3 or Strategy4 as observers of the other players
+        for (Player player : this.players) {
+            for (Player otherPlayer : this.players) {
+                if (otherPlayer != player && (otherPlayer.getPlayerType().equals(PlayerType.PLAYER3) || otherPlayer.getPlayerType().equals(PlayerType.PLAYER4))) {
+                    player.registerObserver(otherPlayer);
+                }
+            }
         }
     }
 
     private void playWithConsole() {
         System.out.println("=== RUMMIKUB ===");
+        System.out.println("\n--- SETUP ---");
         this.setup();
 
+        System.out.println("\n--- GAME ---");
         Tile tile; // Used to store the tile drawn from the stock.
         ArrayList<Meld> workspace; // Store the player's workspace.
         String winner = null;
@@ -104,7 +80,7 @@ public class Game {
                 System.out.println("Table:\n" + this.table.toString());
                 System.out.println(player.getName() + "'s turn");
 
-                workspace = player.play();
+                workspace = player.play(this.table.getState());
                 if (workspace == null) {
                     tile = this.stock.draw();
                     player.add(tile);
@@ -138,5 +114,67 @@ public class Game {
         }
 
         System.out.println(winner + " has won!");
+    }
+    
+    protected void createPlayers() {
+        this.players = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int playerCount = 0;
+        while (playerCount < 4) {
+            System.out.println("Enter name for Player " + (playerCount + 1) + ":");
+            String name = scanner.nextLine();
+            System.out.println("Which strategy should " + name + " use:");
+            System.out.println("1. StrategyHuman");
+            System.out.println("2. Strategy1");
+            System.out.println("3. Strategy2");
+            System.out.println("4. Strategy3");
+            System.out.println("5. Strategy4");
+            
+            int choice = 0;
+            Player player = null;
+            
+            while (choice < 1 || choice > 5) {
+                // Validate input is an integer
+                while (!scanner.hasNextInt()) {
+                    System.out.println("Invalid option (that's not an integer).");
+                    scanner.next();
+                }
+                choice = scanner.nextInt();
+                scanner.nextLine();
+                
+                // Validate integer input is within valid range
+                if (choice < 1 || choice > 5 ) {
+                    System.out.println("Invalid option.");
+                }
+            }
+
+            if (choice == 1) {
+                player = new PlayerHuman(name);
+            } else if (choice == 2) {
+                player = new Player1(name);
+            } else if (choice == 3) {
+                player = new Player2(name);
+            } else if (choice == 4) {
+                player = new Player3(name);
+            } else {
+                player = new Player4(name);
+            } 
+ 
+            this.players.add(player);
+            playerCount++;
+            System.out.println(name + " created, uses " + player.getPlayerType().toString() + ".\n");
+            
+            // Prompt user regarding whether they want to add Player 3 and Player 4
+            if (playerCount >= 2 && playerCount < 4) {
+                String addAnother = "";
+                while (!addAnother.equals("y") && !addAnother.equals("n")) {
+                    System.out.println("Would you like to add a Player " + (playerCount + 1) + "? (y/n)");
+                    addAnother = scanner.nextLine();
+                }
+                if (addAnother.equals("n")) {
+                    break;
+                }
+            }
+        }
     }
 }
