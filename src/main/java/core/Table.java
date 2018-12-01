60232e2c0d4bdec6bfb97631b3ada9070c85673e
LayneKM <laynemikan@gmail.com>
2018-12-01 08:55:31 -0500

    [code:Table] Implement copy constructor, setState no longer checks validity, isValidState now public and refers to this.melds

--- src/main/java/core/Table.java
+++ src/main/java/core/Table.java
@@ -8,29 +8,36 @@ public class Table {
     public Table() {
         this.melds = new ArrayList<>();
     }
+    
+    public Table(Table table) {
+        this.melds = new ArrayList<>();
+        for (Meld meld : table.getState()) {
+            Meld newMeld = new Meld();
+            for (int i = 0; i < meld.getSize(); i++) {
+                newMeld.addTile(new Tile(meld.getTile(i).toString()));
+            }
+            melds.add(newMeld);
+        }
+    }
 
     public ArrayList<Meld> getState() {
         return this.melds;
     }
 
-    public boolean setState(ArrayList<Meld> melds) {
-        if (isValidState(melds)) {
-            for (Meld meld : melds) {
-                meld.setIsLocked(true);
-            }
-            this.melds = melds;
-            return true;
+    public void setState(ArrayList<Meld> melds) {
+        for (Meld meld : melds) {
+            meld.setIsLocked(true);
         }
-        return false;
+        this.melds = melds;
     }
 
-    private boolean isValidState(ArrayList<Meld> melds) {
+    public boolean isValidState() {
         // Discard empty melds before validating them
-        melds.removeIf(m -> m.getSize() == 0);
+        this.melds.removeIf(m -> m.getSize() == 0);
 
         // Check if the melds are valid
-        for (int i = 0; i < melds.size(); i++) {
-            if (!melds.get(i).isValidMeld()) {
+        for (int i = 0; i < this.melds.size(); i++) {
+            if (!this.melds.get(i).isValidMeld()) {
                 return false;
             }
         }
