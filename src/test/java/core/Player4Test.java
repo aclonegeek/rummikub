4e30ddb3a5e79fa9be052e7c3f9cc5cfd8a14417
LayneKM <laynemikan@gmail.com>
2018-12-03 13:09:59 -0500

    [test:Player4] Refactor and add test cases to support Strategy4 redesign

--- src/test/java/core/Player4Test.java
+++ src/test/java/core/Player4Test.java
@@ -28,52 +28,48 @@ public class Player4Test extends TestCase {
         assertEquals("[{G1 G2 G3}, {R10 R11 J}]", p4.play(workspace1).toString());
     }
 
-    // Test case where player uses Strategy2 (only plays to existing melds), ie. no other player has 3 fewer tiles
     public void testRegularMove1() {
         Player p4 = new Player4();
         ArrayList<Meld> workspace2 = new ArrayList<>();
         workspace2.add(new Meld("G1,G2,G3"));
         Hand hand2 = new Hand("R1,R2,R3,R4");
         p4.setInitialMove(false);
-        p4.setLowestHandCount(4);
         p4.setHand(hand2);
-        assertEquals("[{G1 G2 G3}, {R1 R2 R3}]", p4.play(workspace2).toString());
+        assertEquals("[{G1 G2 G3}, {R1 R2 R3 R4}]", p4.play(workspace2).toString());
     }
 
-    // Test case where player uses Strategy1 (plays all possible melds), ie. some player has 3 or more fewer tiles
     public void testRegularMove2() {
         Player p4 = new Player4();
         ArrayList<Meld> workspace3 = new ArrayList<>();
         workspace3.add(new Meld("G1,G2,G3"));
         Hand hand3 = new Hand("B10,G10,O10,G4,B10");
         p4.setInitialMove(false);
-        p4.setLowestHandCount(1);
         p4.setHand(hand3);
         assertEquals("[{G1 G2 G3 G4}, {B10 G10 O10}]", p4.play(workspace3).toString());
     }
-
-    // Test case where player does not play any tiles
-    public void testRegularMove3() {
-        Player p4 = new Player4();
-        ArrayList<Meld> workspace5 = new ArrayList<>();
-        workspace5.add(new Meld("G1,G2,G3"));
-        Hand hand5 = new Hand("B10,G10,O10,R10,R5,R6,R7,R8");
-        p4.setInitialMove(false);
-        p4.setLowestHandCount(8);
-        p4.setHand(hand5);
-        assertEquals(null, p4.play(workspace5));
-    }
     
     // Test case where player plays joker
-    public void testRegularMove4() {
+    public void testRegularMove3() {
         Player p4 = new Player4();
         ArrayList<Meld> workspace3 = new ArrayList<>();
         workspace3.add(new Meld("G1,G2,G3"));
         Hand hand3 = new Hand("B10,G10,O10,G4,B10,J");
         p4.setInitialMove(false);
-        p4.setLowestHandCount(1);
         p4.setHand(hand3);
         assertEquals("[{G1 G2 G3 G4}, {B10 G10 O10 J}]", p4.play(workspace3).toString());
+    }
+    
+    // Test case where player uses prioritization
+    public void testRegularMove4() {
+        Player p4 = new Player4();
+        ArrayList<Meld> workspace = new ArrayList<>();
+        workspace.add(new Meld("R1,R2,R3"));
+        workspace.add(new Meld("G2,R2,B2"));
+        workspace.add(new Meld("R9,R10,R11"));
+        Hand hand = new Hand("R12,B2,G2,O2,B3,B4,B5,B6,O1,O2");
+        p4.setInitialMove(false);
+        p4.setHand(hand);
+        assertEquals("[{R1 R2 R3}, {G2 R2 B2 O2}, {R9 R10 R11 R12}, {B2 G2 O2}, {B3 B4 B5 B6}]", p4.play(workspace).toString());
     }
 
     public void testToString() {
